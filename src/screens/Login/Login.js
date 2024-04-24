1daa6ed9d83caf97e96d8730568b13bdd415237e
EndLess728 <aryanmk.mk5@gmail.com>
2024-04-24 11:58:05 +0530

    Add expo-dev-client

--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,10 +1,8 @@
-import { ActivityIndicator, StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { fonts } from "@/theme";
-import Constants from "expo-constants";
 import i18n from "@/localization/i18n";
 export const Login = () => {
-
   return (
     <View
       style={{
@@ -21,11 +19,10 @@ export const Login = () => {
           fontFamily: fonts.openSan.boldItalic,
         }}
       >
-        {i18n.t('welcome')}
+        {i18n.t("welcome")}
       </Text>
     </View>
   );
 };
-
 
 const styles = StyleSheet.create({});

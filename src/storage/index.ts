36ff40ec46ac0d112f3ec1da8ce66d67bb5cc377
EndLess728 <aryanmk.mk5@gmail.com>
2025-12-18 20:46:54 +0530

    chore(mmkv): add storage universal methods

--- src/storage/index.ts
+++ src/storage/index.ts
@@ -6,3 +6,16 @@ export const storage = createMMKV({
   id: 'secureStorage',
   encryptionKey: 'my-super-secret-key', // Use a secure secret key to encrypt the storage
 });
+
+export function getItem<T>(key: string): T | null {
+  const value = storage.getString(key);
+  return value ? JSON.parse(value) || null : null;
+}
+
+export function setItem<T>(key: string, value: T) {
+  storage.set(key, JSON.stringify(value));
+}
+
+export function removeItem(key: string) {
+  storage.remove(key);
+}

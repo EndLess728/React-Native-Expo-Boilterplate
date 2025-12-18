6e2b72dea9698ecdf218eb33545f2ece47f7c08b
Mantu <mantu.kumar@itechnolabs.tech>
2025-12-18 20:46:19 +0530

    chore: force unistyle stylesheet api and syntax

--- eslint.config.js
+++ eslint.config.js
@@ -103,6 +103,28 @@ module.exports = [
           reservedFirst: true,
         },
       ],
+      'no-restricted-imports': [
+        'error',
+        {
+          paths: [
+            {
+              name: 'react-native',
+              importNames: ['StyleSheet'],
+              message:
+                "Please use unistyles StyleSheet instead. import { StyleSheet } from 'react-native-unistyles'",
+            },
+          ],
+        },
+      ],
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector:
+            "CallExpression[callee.object.name='StyleSheet'][callee.property.name='create'] > ObjectExpression",
+          message:
+            'StyleSheet.create should use a callback function: StyleSheet.create(() => ({ ... }))',
+        },
+      ],
     },
   },
 
